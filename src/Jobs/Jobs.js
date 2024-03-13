77780c68c64e163333dd3943e2f063a8fa8bdbbb
Sidd@2124 <siddhumsd@gmail.com>
2024-03-13 20:16:42 +0530

    Updates

--- src/Jobs/Jobs.js
+++ src/Jobs/Jobs.js
@@ -169,12 +169,7 @@ const Jobs =()=>{
                 <div className='OpeningsHeading'>
                     <div>
              <h1 className='CurreniOpenings'>{FilterdJobs.length} Jobs Found for you...</h1>
-
-             <input type='Search' placeholder='Job Title or Key word' value={SeachInputValue} onChange={UpdateInputValue}/>
-             <svg onClick={UpdateSearchResult} xmlns="http://www.w3.org/2000/svg" className='SerachIcon' viewBox="0 0 24 24" width="39" height="34" fill="currentColor"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path></svg>
-             </div>
-           
-            <div>
+             <div>
                 <h3 >Choose By Country</h3>
              <section>
                 <select onChange={UpdateCountry}>
@@ -191,6 +186,29 @@ const Jobs =()=>{
                 </select>
              </section>
              </div>
+             
+             </div>
+             <div className="search-container">
+    <input 
+        type='Text' 
+        placeholder='Job Title or Keyword' 
+      value={SeachInputValue}
+        onChange={UpdateInputValue}
+    />
+    <svg 
+        onClick={UpdateSearchResult} 
+        xmlns="http://www.w3.org/2000/svg" 
+        className='search-icon' 
+        viewBox="0 0 24 24" 
+        fill="currentColor"
+    >
+        <path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path>
+    </svg>
+</div>
+
+
+           
+           
              </div>
         <div >
             {FilterdJobs.length===0?
